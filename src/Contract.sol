2795fc6f4d8b6096cffd2fad59658d3ccf5ca0ba
Temirzhan Yussupov <timka2609@gmail.com>
2022-06-19 18:48:54 +0800

    feat: update solc version

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+contract Contract {
+    address public owner;
+
+    constructor(address[] memory users) {
+        owner = users[0];
+    }
+}
